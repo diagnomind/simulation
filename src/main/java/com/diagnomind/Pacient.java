77308b80a039ff60cc58850020c48b3ea4ac9a12
Txillu <qingyu.jiang@alumni.mondragon.edu>
2023-12-19 08:37:01 +0100

    attributes for time

--- src/main/java/com/diagnomind/Pacient.java
+++ src/main/java/com/diagnomind/Pacient.java
@@ -1,16 +1,38 @@
 package com.diagnomind;
 
-public class Pacient  extends Thread {
+public class Pacient extends Thread {
 
     int diagnosisId;
     String name;
     Hospital hospital;
+    long tiempoInit;
+    long tiempoFin;
 
-    public Pacient(String name, int id,Hospital hospital){
+    public Pacient(String name, int id, Hospital hospital) {
         super("Pacient");
-        this.name=name;
-        this.hospital=hospital;
-        this.diagnosisId=id;
+        this.name = name;
+        this.hospital = hospital;
+        this.diagnosisId = id;
+    }
+
+    public long getTiempoInit() {
+        return tiempoInit;
+    }
+
+    public void setTiempoInit(long tiempoInit) {
+        this.tiempoInit = tiempoInit;
+    }
+
+    public long getTiempoFin() {
+        return tiempoFin;
+    }
+
+    public void setTiempoFin(long tiempoFin) {
+        this.tiempoFin = tiempoFin;
+    }
+
+    public long calcularTiempoEjecucion(){
+        return (this.tiempoFin-this.tiempoInit);
     }
 
     @Override
