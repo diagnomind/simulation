4d26615f4b7069536956e1ce87a3a7309bb4f97a
Txillu <qingyu.jiang@alumni.mondragon.edu>
2024-01-10 12:19:13 +0100

    Tests for pacient Class

---
+++ src/test/java/com/diagnomind/simulation/PacientTest.java
@@ -0,0 +1,46 @@
+package com.diagnomind.simulation;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PacientTest {
+
+    private Patient pacient;
+
+    @Before
+    public void constructorTest() {
+        String name = "Pacient test";
+        Hospital hospital = new Hospital();
+        int id = 0;
+        pacient = new Patient(name, id, hospital);
+    }
+
+    @Test
+    public void tiempoInitTest() {
+        pacient.setTiempoInit(1);
+        assertEquals(1, pacient.getTiempoInit());
+        ;
+    }
+
+    @Test
+    public void tiempoFinTest() {
+        pacient.setTiempoFin(1);
+        assertEquals(1, pacient.getTiempoFin());
+        ;
+    }
+
+    @Test
+    public void calcularTiempoEjecucionTest() {
+        pacient.setTiempoInit(0);
+        pacient.setTiempoFin(5);
+        assertEquals(5, pacient.calcularTiempoEjecucion());
+    }
+
+    // @Test()
+    // public void runTest() {
+      
+    // }
+}
