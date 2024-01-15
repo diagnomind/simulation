8e1d7c90c4fbc2f2254e138e293c00075175e49d
Txillu <qingyu.jiang@alumni.mondragon.edu>
2024-01-15 21:36:03 +0100

    run test Specialist

--- src/test/java/com/diagnomind/simulation/SpecialistTest.java
+++ src/test/java/com/diagnomind/simulation/SpecialistTest.java
@@ -1,8 +1,13 @@
 package com.diagnomind.simulation;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 
+import java.lang.annotation.Target;
+
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class SpecialistTest {
     
@@ -13,6 +18,32 @@ public class SpecialistTest {
     public void setup() {
         hospitalMock = mock(Hospital.class);
         specialistTest = new Specialist(hospitalMock, 1);
+    }
+
+    @After
+    public void clear(){
+        hospitalMock=null;
+    }
+
+    @Test
+    public void runTest(){
+        specialistTest.start();
+           try {
+        // Esperar un tiempo corto para darle al hilo la oportunidad de ejecutar algunas
+        // iteraciones
+            Thread.sleep(5000);
+            
+            specialistTest.interrupt();
+
+            // Esperar a que el hilo termine
+            specialistTest.join();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        specialistTest.interrupt();
+        assertTrue(specialistTest.isInterrupted());
+
     }
 
 }
