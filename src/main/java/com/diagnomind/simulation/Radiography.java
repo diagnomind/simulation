81237de2a11c7e4d6f8d600048c8111e3e16af30
Diogo_S <97432225+DiosGod@users.noreply.github.com>
2024-01-19 17:52:34 +0100

    Update Radiography.java

--- src/main/java/com/diagnomind/simulation/Radiography.java
+++ src/main/java/com/diagnomind/simulation/Radiography.java
@@ -3,11 +3,9 @@ package com.diagnomind.simulation;
 public class Radiography {
 
     Patient patient;
-    Diagnosis diagnosis;
 
-    public Radiography(Diagnosis diagnosis) {
-        this.diagnosis = diagnosis;
-        this.patient = diagnosis.getPatient();
+    public Radiography(Patient patient) {
+        this.patient = patient;
     }
 
 }
